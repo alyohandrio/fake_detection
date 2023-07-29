11432aa6ce73023125b463e16d739e5c45bf228b
alyohandrio <alexdanilyu@gmail.com>
2023-07-29 23:57:33 +0300

    Add predict function

--- processing.py
+++ processing.py
@@ -7,28 +7,29 @@ import torch
 from feature_extraction import ImageDataset
 
 
-images_path = os.path.join("images", "data")
-head_path = os.path.join("checkpoints", "head.pth")
+def predict_fakes(images_path, head_path="head.pth")
+    head_path = os.path.join("checkpoints", head_pth)
+    weights = ViT_L_16_Weights.IMAGENET1K_V1
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    vit = vit_l_16(weights=weights).to(device)
+    vit.eval()
 
-weights = ViT_L_16_Weights.IMAGENET1K_V1
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-vit = vit_l_16(weights=weights).to(device)
-vit.eval()
+    head = torch.nn.Linear(in_features=1000, out_features=2, device=device)
+    head.load_state_dict(torch.load(head_path))
+    head.eval()
 
-head = torch.nn.Linear(in_features=1000, out_features=2, device=device)
-head.load_state_dict(torch.load(head_path))
-head.eval()
+    transform = weights.transforms()
 
-transform = weights.transforms()
+    dataset = ImageDataset(images_path, transform)
+    loader = DataLoader(dataset, batch_size=256, shuffle=False)
 
-dataset = ImageDataset(images_path, transform)
-loader = DataLoader(dataset, batch_size=256, shuffle=False)
+    saved = 0
+    predictions = torch.tensor([]).to(device)
+    for images in loader:
+        images = images.to(device)
+        with torch.no_grad():
+            features = vit(images)
+            output = head(features).argmax(dim=1)
+            predictions = torch.cat((predictions, output))
+    return predictions
 
-saved = 0
-predictions = torch.tensor([]).to(device)
-for images in loader:
-    images = images.to(device)
-    with torch.no_grad():
-        features = vit(images)
-        output = head(features).argmax(dim=1)
-        predictions = torch.cat((predictions, output))
